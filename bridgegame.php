6f8a4918c2f3d1c2d3a080ea0010757c07c1bead
Steven van Dijk <zteven@gmail.com>
2023-07-24 16:18:49 +0200

    Update bridgegame.php

--- bridgegame.php
+++ bridgegame.php
@@ -4,7 +4,7 @@
 Plugin Name: BridgeGame
 Plugin URI: https://example1.com
 Description: A fun game to introduce bridge
-Version: 1.0
+Version: 1.{VersionCounter}
 Author: Pepijn van der Valk
 Author URI: https://example2.com
 License: GPL2
